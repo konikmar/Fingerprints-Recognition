332d4d4be632a9e9745cc9ae802054eedd2ad9be
Maciej <maciej.j.skrzypczak@student.put.poznan.pl>
2017-08-20 17:37:46 +0200

    Guo-Hall Fixed

--- mgr/mgr/Thinning.cpp
+++ mgr/mgr/Thinning.cpp
@@ -127,8 +127,7 @@ void Thinning::ThinningNegative1(const Mat& src, Mat& dst){
 Thinning::~Thinning(){
 }
 
-void Thinning::ZhangSuenThinning(const Mat& src, Mat& dst) // na podstawie: https://rosettacode.org/wiki/Zhang-Suen_thinning_algorithm
-// tak jak mowilem, dziala troche dziwnie, duzo z tym kombinowalem ale jeszcze nie zdiagnozowalem dlaczego nie dziala tak samo jak ten z internetu (moze to byc jakas prosta rzecz, ktorej nie widze)
+void Thinning::ZhangSuenThinning(const Mat& src, Mat& dst)
 {
 	dst = src.clone();
 	Negative(dst);
@@ -157,9 +156,6 @@ void Thinning::ZhangSuenThinning(const Mat& src, Mat& dst) // na podstawie: http
 
 				P1 = dst.at<uchar>(i, j);
 
-				///if (P1 == 1)
-				///{
-
 					P2 = dst.at<uchar>(i - 1, j);
 					P3 = dst.at<uchar>(i - 1, j + 1);
 					P4 = dst.at<uchar>(i, j + 1);
@@ -168,27 +164,6 @@ void Thinning::ZhangSuenThinning(const Mat& src, Mat& dst) // na podstawie: http
 					P7 = dst.at<uchar>(i + 1, j - 1);
 					P8 = dst.at<uchar>(i, j - 1);
 					P9 = dst.at<uchar>(i - 1, j - 1);
-				
-
-					////count A
-					//if (P2 == 0 && P3 == 1) A++;
-					//if (P3 == 0 && P4 == 1) A++;
-					//if (P4 == 0 && P5 == 1) A++;
-					//if (P5 == 0 && P6 == 1) A++;
-					//if (P6 == 0 && P7 == 1) A++;
-					//if (P7 == 0 && P8 == 1) A++;
-					//if (P8 == 0 && P9 == 1) A++;
-					//if (P9 == 0 && P2 == 1) A++;
-
-					////count B
-					//if (P2 == 1) B++;
-					//if (P3 == 1) B++;
-					//if (P4 == 1) B++;
-					//if (P5 == 1) B++;
-					//if (P6 == 1) B++;
-					//if (P7 == 1) B++;
-					//if (P8 == 1) B++;
-					//if (P9 == 1) B++;
 
 					A = (P2 == 0 && P3 == 1) + (P3 == 0 && P4 == 1) +
 						(P4 == 0 && P5 == 1) + (P5 == 0 && P6 == 1) +
@@ -201,7 +176,6 @@ void Thinning::ZhangSuenThinning(const Mat& src, Mat& dst) // na podstawie: http
 
 					if (A == 1 && (B >= 2 && B <= 6) && m1==0 && m2 == 0)
 					{
-						//dst.at<uchar>(i, j) = 0;
 						pDst[j] = 1;
 						change = true;
 					}
@@ -223,9 +197,6 @@ void Thinning::ZhangSuenThinning(const Mat& src, Mat& dst) // na podstawie: http
 
 				P1 = dst.at<uchar>(i, j);
 
-				///if (P1 == 1)
-				///{
-
 					P2 = dst.at<uchar>(i - 1, j);
 					P3 = dst.at<uchar>(i - 1, j + 1);
 					P4 = dst.at<uchar>(i, j + 1);
@@ -235,26 +206,6 @@ void Thinning::ZhangSuenThinning(const Mat& src, Mat& dst) // na podstawie: http
 					P8 = dst.at<uchar>(i, j - 1);
 					P9 = dst.at<uchar>(i - 1, j - 1);
 
-
-					////count A
-					//if (P2 == 0 && P3 == 1) A++;
-					//if (P3 == 0 && P4 == 1) A++;
-					//if (P4 == 0 && P5 == 1) A++;
-					//if (P5 == 0 && P6 == 1) A++;
-					//if (P6 == 0 && P7 == 1) A++;
-					//if (P7 == 0 && P8 == 1) A++;
-					//if (P8 == 0 && P9 == 1) A++;
-					//if (P9 == 0 && P2 == 1) A++;
-
-					////count B
-					//if (P2 == 1) B++;
-					//if (P3 == 1) B++;
-					//if (P4 == 1) B++;
-					//if (P5 == 1) B++;
-					//if (P6 == 1) B++;
-					//if (P7 == 1) B++;
-					//if (P8 == 1) B++;
-					//if (P9 == 1) B++;
 
 					A = (P2 == 0 && P3 == 1) + (P3 == 0 && P4 == 1) +
 						(P4 == 0 && P5 == 1) + (P5 == 0 && P6 == 1) +
@@ -267,7 +218,6 @@ void Thinning::ZhangSuenThinning(const Mat& src, Mat& dst) // na podstawie: http
 
 					if (A == 1 && (B >= 2 && B <= 6) && m1 == 0 && m2 == 0)
 					{
-						//dst.at<uchar>(i, j) = 0;
 						pDst[j] = 1;
 						change = true;
 					}
@@ -293,46 +243,51 @@ void Thinning::ZhangSuenThinning(const Mat& src, Mat& dst) // na podstawie: http
 	
 }
 
-void Thinning::GuoHallThinning(const Mat& src, Mat& dst) //tez na razie dziala troche dziwnie (i dlugo) - poki co jest na bazie tego co tu: http://answers.opencv.org/question/3207/what-is-a-good-thinning-algorithm-for-getting-the-skeleton-of-characters-for-ocr/
-														 //ale widze, ze tu sa niezle rozpisane algorytmy, wiec pozniej sprobuje zrobic na podstawie tego: http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.232.1226&rep=rep1&type=pdf 
-{	
-	src.copyTo(dst);
+void Thinning::GuoHallThinning(const Mat& src, Mat& dst)
+{
+	dst = src.clone();
 	Negative(dst);
-	bool change = true;
-
-	int P1, P2, P3, P4, P5, P6, P7, P8, P9;
-	
 	dst /= 255;
-
+	int P1, P2, P3, P4, P5, P6, P7, P8, P9;
+	int M;
+	int C = 0; // number of transitions from 255 to 0 in the sequence P2,P3,P4,P5,P6,P7,P8,P9,P2
+	int N = 0; // number of black pixel neighbours of P1
+	bool change = true;
 	cv::Mat prev = cv::Mat::zeros(dst.size(), CV_8UC1);
 	cv::Mat diff;
-
-	do
+	cv::Mat marker = cv::Mat::zeros(dst.size(), CV_8UC1);
+	uchar *pDst;
+	int d = 1;
+	while (cv::countNonZero(diff) > 0 || d == 1)
 	{
-		// STEP 1
-		for (int i = 1; i < src.rows - 1; i++)
-		{
-			for (int j = 1; j < src.cols - 1; j++)
-			{
-				//if (src.at<uchar>(i, j) == 255)
-				//{
-					P2 = src.at<uchar>(i, j - 1);
-					P3 = src.at<uchar>(i + 1, j - 1);
-					P4 = src.at<uchar>(i + 1, j);
-					P5 = src.at<uchar>(i + 1, j + 1);
-					P6 = src.at<uchar>(i, j + 1);
-					P7 = src.at<uchar>(i - 1, j + 1);
-					P8 = src.at<uchar>(i - 1, j);
-					P9 = src.at<uchar>(i - 1, j - 1);
+		change = false;
 
-					int C = int(~P8 & (P7 | P6)) +
-						int(~P6 & (P5 | P4)) +
-						int(~P4 & (P3 | P2)) +
-						int(~P2 & (P9 | P8));
+		// STEP 1
+		for (int i = 1; i < dst.rows - 1; ++i)
+		{
+			pDst = marker.ptr<uchar>(i);
+
+			for (int j = 1; j < dst.cols - 1; ++j)
+			{
+
+				P1 = dst.at<uchar>(i, j);
+
+				P2 = dst.at<uchar>(i - 1, j);
+				P3 = dst.at<uchar>(i - 1, j + 1);
+				P4 = dst.at<uchar>(i, j + 1);
+				P5 = dst.at<uchar>(i + 1, j + 1);
+				P6 = dst.at<uchar>(i + 1, j);
+				P7 = dst.at<uchar>(i + 1, j - 1);
+				P8 = dst.at<uchar>(i, j - 1);
+				P9 = dst.at<uchar>(i - 1, j - 1);
+
+				int C = int(~P8 & (P7 | P6)) +
+					int(~P6 & (P5 | P4)) +
+					int(~P4 & (P3 | P2)) +
+					int(~P2 & (P9 | P8));
 
 				if (C == 1)
 				{
-					/// calculate N
 					int N1 = int(P9 | P8) +
 						int(P7 | P6) +
 						int(P5 | P4) +
@@ -344,37 +299,37 @@ void Thinning::GuoHallThinning(const Mat& src, Mat& dst) //tez na razie dziala t
 					int N = min(N1, N2);
 					if ((N == 2) || (N == 3))
 					{
-						/// calculate criteria 3
 						int c3 = (P8 | P7 | ~P5) & P6;
 						if (c3 == 0)
 						{
-							dst.at<uchar>(i, j) = 0;
+							pDst[j] = 1;
 							change = true;
 						}
 					}
 				}
-				//}
+
 			}
 		}
+		dst &= ~marker;
 
 		// STEP 2
-		for (int i = 1; i < src.rows - 1; i++)
+		for (int i = 1; i < dst.rows - 1; ++i)
 		{
-			for (int j = 1; j < src.cols - 1; j++)
+			pDst = marker.ptr<uchar>(i);
+			for (int j = 1; j < dst.cols - 1; ++j)
 			{
-				/*if (src.at<uchar>(i, j) == 255)
-				{*/
-				/// get 8 neighbors
-				/// calculate C(p)
 
-				P2 = src.at<uchar>(i, j - 1);
-				P3 = src.at<uchar>(i + 1, j - 1);
-				P4 = src.at<uchar>(i + 1, j);
-				P5 = src.at<uchar>(i + 1, j + 1);
-				P6 = src.at<uchar>(i, j + 1);
-				P7 = src.at<uchar>(i - 1, j + 1);
-				P8 = src.at<uchar>(i - 1, j);
-				P9 = src.at<uchar>(i - 1, j - 1);
+				P1 = dst.at<uchar>(i, j);
+
+				P2 = dst.at<uchar>(i - 1, j);
+				P3 = dst.at<uchar>(i - 1, j + 1);
+				P4 = dst.at<uchar>(i, j + 1);
+				P5 = dst.at<uchar>(i + 1, j + 1);
+				P6 = dst.at<uchar>(i + 1, j);
+				P7 = dst.at<uchar>(i + 1, j - 1);
+				P8 = dst.at<uchar>(i, j - 1);
+				P9 = dst.at<uchar>(i - 1, j - 1);
+			
 
 				int C = int(~P8 & (P7 | P6)) +
 					int(~P6 & (P5 | P4)) +
@@ -383,7 +338,6 @@ void Thinning::GuoHallThinning(const Mat& src, Mat& dst) //tez na razie dziala t
 
 				if (C == 1)
 				{
-					/// calculate N
 					int N1 = int(P9 | P8) +
 						int(P7 | P6) +
 						int(P5 | P4) +
@@ -398,19 +352,24 @@ void Thinning::GuoHallThinning(const Mat& src, Mat& dst) //tez na razie dziala t
 						int E = (P4 | P3 | ~P9) & P2;
 						if (E == 0)
 						{
-							dst.at<uchar>(i, j) = 0;
+							pDst[j] = 1;
 							change = true;
 						}
 					}
 				}
-				//}
 			}
 		}
+		dst &= ~marker;
 
 		cv::absdiff(dst, prev, diff);
+		if (cv::countNonZero(diff) == 0)
+		{
+			d = 0;
+		}
 		dst.copyTo(prev);
 
-	} while (cv::countNonZero(diff) > 0);
+	}
 
 	dst *= 255;
 }
+
